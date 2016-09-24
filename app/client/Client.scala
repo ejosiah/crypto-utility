bfe7b8929f1056fbf3a4dfc36efefa1ac7c9f89b
Josiah Ebhomenye <josia.ebhomenye@news.co.uk>
2016-09-24 09:26:11 +0100

    added client registery

--- app/client/Client.scala
+++ app/client/Client.scala
@@ -10,13 +10,18 @@ import client.protocol.{EventEncoder, EventDecoder}
 import com.cryptoutility.protocol.Events._
 import play.api.libs.streams.ActorFlow
 import play.api.Logger
+import scala.collection.mutable
 import scala.language.postfixOps
 
 object Client{
 
+  case class Online(user: UserInfo)
+  case class OffLine(user: UserInfo)
+  case class Get(clientId: String)
+
   def props(out: ActorRef) = Props(classOf[Client], out)
 
-  def proxy(client: ActorRef) = Props(classOf[ClientProxy], client)
+  def proxy(client: ActorRef) = Props(classOf[ClientRegistry], client)
 
   def flow(implicit factory: ActorRefFactory, mat: Materializer): Flow[Event, Event, _] = {
     ActorFlow.actorRef(props, overflowStrategy = OverflowStrategy.fail)
@@ -27,31 +32,34 @@ object Client{
   }
 }
 
-class Client(out: ActorRef) extends Actor{
-
+class Client(out: ActorRef, registry: ActorRef) extends Actor{
+  import Client._
   implicit val ec = context.system.dispatcher
+  var mayBeUser = Option.empty[UserInfo]
 
   override def preStart = Logger.info(s"$self connected to server")
 
 
   override def receive = {
-    case Initialized(isNew, userInfo) =>
-      val user = if(isNew){
-        val clientId = UUID.randomUUID().toString
-        userInfo.copy(clientId = Some(clientId))
-      }else{
-        userInfo
-      }
-      context.system.actorOf(Client.proxy(self), s"client-${user.clientId}")
-      out ! UserCreated(user)
+    case Initialized(isNew, user) =>
+      mayBeUser = Some(user)
+      registry ! Online(user)
+    case msg => out ! msg
   }
 
 
   override def postStop(): Unit = Logger.info(s"$self disconnected from server")
 }
 
-class ClientProxy(client: ActorRef) extends Actor{
+class ClientRegistry(client: ActorRef) extends Actor{
+  import Client._
+
+  var users = mutable.Map[String,  (UserInfo, ActorRef)]()
+
   override def receive = {
+    case Online(user) => users += user.clientId.get -> (user -> sender())
+    case OffLine(user) => users -= user.clientId.get
+    case Get(clientId) => sender() ! users.get(clientId)
     case msg => client ! msg
   }
 }
